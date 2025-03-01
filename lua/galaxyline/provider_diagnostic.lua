d325f2e8a4576de84a9184f3b100af105db7c824
Rajkumar Natarajan <rajkumar.natarajan@ekata.com>
2025-02-28 23:31:07 -0800

    removed deprecated

--- lua/galaxyline/provider_diagnostic.lua
+++ lua/galaxyline/provider_diagnostic.lua
@@ -1,12 +1,12 @@
-local vim,lsp,api,diagnostic = vim,vim.lsp,vim.api,vim.diagnostic
+local vim, lsp, diagnostic = vim, vim.lsp, vim.api, vim.diagnostic
 local M = {}
 
 -- coc diagnostic
 local function get_coc_diagnostic(diag_type)
-  local has_info,info = pcall(vim.api.nvim_buf_get_var,0,'coc_diagnostic_info')
+  local has_info, info = pcall(vim.api.nvim_buf_get_var, 0, 'coc_diagnostic_info')
   if not has_info then return end
   if info[diag_type] > 0 then
-    return  info[diag_type]
+    return info[diag_type]
   end
   return ''
 end
@@ -14,8 +14,8 @@ end
 -- nvim-lspconfig
 -- see https://github.com/neovim/nvim-lspconfig
 local function get_nvim_lsp_diagnostic(diag_type)
-  if next(lsp.buf_get_clients(0)) == nil then return '' end
-  local active_clients = lsp.get_active_clients()
+  if next(lsp.get_clients()) == nil then return '' end
+  local active_clients = lsp.get_clients()
 
   if active_clients then
     local result = diagnostic.get(vim.api.nvim_get_current_buf(), { severity = diag_type })
@@ -26,7 +26,7 @@ end
 function M.get_diagnostic_error()
   if vim.fn.exists('*coc#rpc#start_server') == 1 then
     return get_coc_diagnostic('error')
-  elseif not vim.tbl_isempty(lsp.buf_get_clients(0)) then
+  elseif not vim.tbl_isempty(lsp.get_clients()) then
     return get_nvim_lsp_diagnostic(diagnostic.severity.ERROR)
   end
   return ''
@@ -35,7 +35,7 @@ end
 function M.get_diagnostic_warn()
   if vim.fn.exists('*coc#rpc#start_server') == 1 then
     return get_coc_diagnostic('warning')
-  elseif not vim.tbl_isempty(lsp.buf_get_clients(0)) then
+  elseif not vim.tbl_isempty(lsp.get_clients()) then
     return get_nvim_lsp_diagnostic(diagnostic.severity.WARN)
   end
   return ''
@@ -44,7 +44,7 @@ end
 function M.get_diagnostic_hint()
   if vim.fn.exists('*coc#rpc#start_server') == 1 then
     return get_coc_diagnostic('hint')
-  elseif not vim.tbl_isempty(lsp.buf_get_clients(0)) then
+  elseif not vim.tbl_isempty(lsp.get_clients()) then
     return get_nvim_lsp_diagnostic(diagnostic.severity.HINT)
   end
   return ''
@@ -53,7 +53,7 @@ end
 function M.get_diagnostic_info()
   if vim.fn.exists('*coc#rpc#start_server') == 1 then
     return get_coc_diagnostic('information')
-  elseif not vim.tbl_isempty(lsp.buf_get_clients(0)) then
+  elseif not vim.tbl_isempty(lsp.get_clients()) then
     return get_nvim_lsp_diagnostic(diagnostic.severity.INFO)
   end
   return ''
--- lua/galaxyline/provider_lsp.lua
+++ lua/galaxyline/provider_lsp.lua
@@ -1,14 +1,14 @@
-local get_lsp_client = function (msg)
+local get_lsp_client = function(msg)
   msg = msg or 'No Active Lsp'
-  local buf_ft = vim.api.nvim_buf_get_option(0,'filetype')
-  local clients = vim.lsp.get_active_clients()
+  local buf_ft = vim.api.nvim_get_option_value('filetype', {})
+  local clients = vim.lsp.get_clients()
   if next(clients) == nil then
     return msg
   end
 
-  for _,client in ipairs(clients) do
+  for _, client in ipairs(clients) do
     local filetypes = client.config.filetypes
-    if filetypes and vim.fn.index(filetypes,buf_ft) ~= -1 then
+    if filetypes and vim.fn.index(filetypes, buf_ft) ~= -1 then
       return client.name
     end
   end
